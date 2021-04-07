dff678516ae4135b6c1717f6469819ae87ab3610
ljessicc <ljessicc@gmail.com>
2021-04-07 20:42:47 +0100

    first commit of todo app

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+const DATA = [
+];
+
+
+ReactDOM.render(<App tasks={DATA} />,
+  
   document.getElementById('root')
 );
 
